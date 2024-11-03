41ea9556930911688d07430b75311cf2fea6715e
Tim Dortmann <git@tdortman.com>
2024-11-03 04:41:48 +0100

    streamline things a bit, also make it work with torchrun

---
+++ train.py
@@ -0,0 +1,150 @@
+import glob
+import multiprocessing as mp
+import os
+
+import torch
+from torch.distributed import destroy_process_group, init_process_group
+from torch.optim import AdamW
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.distributed import DistributedSampler
+
+from model import GPTLanguageModel
+from trainer import Trainer  # Import Trainer class from the appropriate module
+
+
+def read_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as file:
+        return file.read() + "\n"
+
+
+def read_all_files_to_string(directory):
+    filepaths = [
+        filepath
+        for filepath in glob.glob(os.path.join(directory, "**", "*"), recursive=True)
+        if os.path.isfile(filepath)
+    ]
+
+    if not filepaths:
+        raise ValueError("No files found in the input directory.")
+
+    combined_string = ""
+    with mp.Pool(min(len(filepaths), mp.cpu_count())) as executor:
+        results = executor.map(read_file, filepaths)
+        combined_string = "".join(results)
+
+    return combined_string
+
+
+def prepare_data(text: str):
+    if not text:
+        raise ValueError(
+            "The input text is empty. Please check the file reading process."
+        )
+
+    lines = text.splitlines()
+    lines = [line for line in lines if all(c.isascii() for c in line)]
+
+    if not lines:
+        raise ValueError("No valid ASCII lines found in the input text.")
+
+    chars = sorted(list(set("".join(lines[:100_000]))))
+    vocab_size = len(chars)
+    stoi = {ch: i for i, ch in enumerate(chars)}
+    itos = {i: ch for i, ch in enumerate(chars)}
+
+    def encode(string):
+        return [stoi[c] for c in string]
+
+    def decode(tokens):
+        return "".join([itos[i] for i in tokens])
+
+    encoded_lines = [torch.tensor(encode(line), dtype=torch.long) for line in lines]
+
+    if not encoded_lines:
+        raise ValueError("No lines were encoded. Check the encoding process.")
+
+    data = torch.cat(encoded_lines)
+
+    n = len(data)
+    train_data = data[: int(n * 0.8)]
+    val_data = data[int(n * 0.8) :]
+
+    return train_data, val_data, encode, decode, vocab_size
+
+
+class TextDataset(Dataset):
+    def __init__(self, data, block_size):
+        self.data = data
+        self.block_size = block_size
+
+    def __len__(self):
+        return len(self.data) - self.block_size
+
+    def __getitem__(self, idx):
+        x = self.data[idx : idx + self.block_size]
+        y = self.data[idx + 1 : idx + self.block_size + 1]
+        return x, y
+
+
+batch_size = 128
+block_size = 128
+max_epochs = 20
+learning_rate = 3e-4
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+save_every = 10
+
+
+text = read_all_files_to_string("data")
+train_data, val_data, encode, decode, vocab_size = prepare_data(text)
+
+
+def ddp_setup():
+    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+    init_process_group(backend="nccl")
+
+
+def load_train_objs():
+    train_dataset = TextDataset(train_data, block_size)
+
+    model = GPTLanguageModel(
+        vocab_size,
+        n_embd,
+        block_size,
+        n_layer,
+        n_head,
+        dropout,
+    )
+
+    return train_dataset, model, AdamW(model.parameters(), lr=learning_rate)
+
+
+def prepare_dataloader(dataset: Dataset, batch_size: int):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        pin_memory=True,
+        shuffle=False,
+        sampler=DistributedSampler(dataset),
+    )
+
+
+def main(
+    save_every: int,
+    total_epochs: int,
+    batch_size: int,
+    snapshot_path: str = "snapshot.pt",
+):
+    ddp_setup()
+    dataset, model, optimizer = load_train_objs()
+    train_loader = prepare_dataloader(dataset, batch_size)
+    print(len(train_loader))
+    trainer = Trainer(model, train_loader, optimizer, save_every, snapshot_path)
+    trainer.train(total_epochs)
+    destroy_process_group()
+
+
+if __name__ == "__main__":
+    main(save_every, max_epochs, batch_size)
---
+++ trainer.py
@@ -0,0 +1,70 @@
+import os
+
+import torch
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        train_data: DataLoader,
+        optimizer: torch.optim.Optimizer,
+        save_every: int,
+        snapshot_path: str,
+    ) -> None:
+        self.gpu_id = int(os.environ["LOCAL_RANK"])
+        self.model = model.to(self.gpu_id)
+        self.train_data = train_data
+        self.optimizer = optimizer
+        self.save_every = save_every
+        self.epochs_run = 0
+        self.snapshot_path = snapshot_path
+        if os.path.exists(snapshot_path):
+            print("Loading snapshot")
+            self._load_snapshot(snapshot_path)
+
+        self.model = DDP(self.model, device_ids=[self.gpu_id])
+
+    def _load_snapshot(self, snapshot_path):
+        loc = f"cuda:{self.gpu_id}"
+        snapshot = torch.load(snapshot_path, map_location=loc)
+        self.model.load_state_dict(snapshot["MODEL_STATE"])
+        self.epochs_run = snapshot["EPOCHS_RUN"]
+        print(f"Resuming training from snapshot at Epoch {self.epochs_run}")
+
+    def _run_batch(self, source, targets):
+        self.optimizer.zero_grad()
+        _, loss = self.model(source, targets)
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+    def _run_epoch(self, epoch: int):
+        b_sz = len(next(iter(self.train_data))[0])
+        self.train_data.sampler.set_epoch(epoch)
+        source, targets = next(iter(self.train_data))
+        source = source.to(self.gpu_id)
+        targets = targets.to(self.gpu_id)
+        loss = self._run_batch(source, targets)
+
+
+        print(
+            f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Loss: {loss:.4f}| Steps: {len(self.train_data)}"
+        )
+
+    def _save_snapshot(self, epoch):
+        snapshot = {
+            "MODEL_STATE": self.model.module.state_dict(),
+            "EPOCHS_RUN": epoch,
+        }
+        torch.save(snapshot, self.snapshot_path)
+        print(f"Epoch {epoch} | Training snapshot saved at {self.snapshot_path}")
+
+    def train(self, max_epochs: int):
+        for epoch in range(self.epochs_run, max_epochs):
+            self._run_epoch(epoch)
+            if self.gpu_id == 0 and (epoch + 1) % self.save_every == 0:
+                self._save_snapshot(epoch)
